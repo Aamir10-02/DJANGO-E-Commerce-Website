2e0c3483d5c8a5fdc4a1d5a86e83087f2f7d0506
Aamir10-02 <aamir2saiyad@gmail.com>
2025-03-25 06:57:09 +0000

    added domain name

--- ecom/settings.py
+++ ecom/settings.py
@@ -17,8 +17,8 @@ SECRET_KEY = 'django-insecure-d&qqo*#fg&*)nvuv858g6_qxa7@i4w4r#1dtw+_1ni9nu0)q^-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-e-commerce-website-production.up.railway.app', 'https://django-e-commerce-website-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS =['https://django-e-commerce-website-production.up.railway.app']
+ALLOWED_HOSTS = ['https://djangoecom.store', 'django-e-commerce-website-production.up.railway.app', 'https://django-e-commerce-website-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS =['https://djangoecom.store', 'https://django-e-commerce-website-production.up.railway.app']
 
 
 # Application definition
